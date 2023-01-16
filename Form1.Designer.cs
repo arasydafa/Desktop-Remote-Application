bb4c099e54d3a0ef456affd2aa946f7c4b5eadf1
Arasy Dafa Sulistya Kurniawan <arasy.dafa@gmail.com>
2023-01-16 15:04:35 +0700

    Add windows function disabled and UDP communication

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,44 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
+            this.executeButton = new System.Windows.Forms.Button();
             this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pathBox = new System.Windows.Forms.TextBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.processChecker = new System.Windows.Forms.Timer(this.components);
             this.processValidation = new System.Windows.Forms.Timer(this.components);
+            this.receiveButton = new System.Windows.Forms.Button();
+            this.dataInBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // executeButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.button1.Location = new System.Drawing.Point(32, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "EXECUTE";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.executeButton.Location = new System.Drawing.Point(32, 78);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(103, 33);
+            this.executeButton.TabIndex = 0;
+            this.executeButton.Text = "EXECUTE";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
             // 
             // OpenFileDialog
             // 
             this.OpenFileDialog.FileName = "OpenFileDialog";
             // 
-            // textBox1
+            // pathBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(170, 84);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(624, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            this.pathBox.Location = new System.Drawing.Point(170, 84);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(624, 20);
+            this.pathBox.TabIndex = 2;
+            this.pathBox.DoubleClick += new System.EventHandler(this.pathBox_DoubleClick);
+            this.pathBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pathBox_KeyDown);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 221);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 263);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(855, 22);
             this.statusStrip1.TabIndex = 3;
@@ -78,18 +82,51 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(45, 17);
             this.toolStripStatusLabel1.Text = "Status: ";
             // 
+            // receiveButton
+            // 
+            this.receiveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.receiveButton.Location = new System.Drawing.Point(32, 135);
+            this.receiveButton.Name = "receiveButton";
+            this.receiveButton.Size = new System.Drawing.Size(103, 33);
+            this.receiveButton.TabIndex = 0;
+            this.receiveButton.Text = "RECEIVE";
+            this.receiveButton.UseVisualStyleBackColor = true;
+            this.receiveButton.Click += new System.EventHandler(this.receiveButton_Click);
+            // 
+            // dataInBox
+            // 
+            this.dataInBox.Location = new System.Drawing.Point(170, 191);
+            this.dataInBox.Name = "dataInBox";
+            this.dataInBox.Size = new System.Drawing.Size(624, 20);
+            this.dataInBox.TabIndex = 2;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.clearButton.Location = new System.Drawing.Point(32, 185);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(103, 33);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "CLEAR";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(855, 243);
+            this.ClientSize = new System.Drawing.Size(855, 285);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataInBox);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.receiveButton);
+            this.Controls.Add(this.executeButton);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -99,13 +136,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button executeButton;
         private System.Windows.Forms.OpenFileDialog OpenFileDialog;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox pathBox;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Timer processChecker;
         private System.Windows.Forms.Timer processValidation;
+        private System.Windows.Forms.Button receiveButton;
+        private System.Windows.Forms.TextBox dataInBox;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,9 @@ using System.Diagnostics;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,65 +15,203 @@ using System.IO;
 using Microsoft.Win32;
 using DevExpress.Internal;
 using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Threading;
 
 namespace DesktopRemoteApplication
 {
     public partial class Form1 : Form
     {
+        /*#region Button Disable variable and Dependencies
+        private static int WH_KEYBOARD_LL = 13;
+        private static int WM_KEYDOWN = 0x0100;
+        private static int WM_SYSKEYDOWN = 0x0104;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        #endregion*/
+
+        #region Form Process Variable and Dependencies
+        private bool _isRunning = false;
+
+        private UdpClient _udpClient;
+        private IPEndPoint _remoteEndPoint;
+        private bool _isListening;
+        #endregion
+
+        #region Form Process 
         public Form1()
         {
             InitializeComponent();
+
+            // _hookID = SetHook(_proc);
         }
 
-        private void textBox1_Click(object sender, EventArgs e)
+        private void StartProcess()
         {
+            if (_isRunning) return;
+            _isRunning = true;
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var program = textBox1.Text.ToString();
+            var program = pathBox.Text.ToString();
 
             try
             {
-                using (var process = new Process())
+                if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(program)).Any(p => p.MainModule.FileName == program))
                 {
-                    process.StartInfo = new ProcessStartInfo(program);
-                    process.Start();
+                    statusStrip1.Items[0].Text = "Status: Application is already running";
+                    MessageBox.Show("Application is already running");
                 }
-
-                string processName = Path.GetFileNameWithoutExtension(program);
-                processChecker.Interval = 100;
-                processChecker.Tick += (s, eventArgs) =>
+                else
                 {
-                    bool isRunning = Process.GetProcessesByName(processName).Any(p => p.MainModule.FileName == program);
-                    if (isRunning)
+                    using (var process = new Process())
                     {
-                        statusStrip1.Items[0].Text = "Status: Application '" + processName + "' Running";
+                        process.StartInfo = new ProcessStartInfo(program);
+                        process.Start();
                     }
-                    else
-                    {
-                        var allProcess = Process.GetProcesses();
 
-                        if (allProcess.Length > 0)
+                    string processName = Path.GetFileNameWithoutExtension(program);
+                    processChecker.Interval = 100;
+                    processChecker.Tick += (s, eventArgs) =>
+                    {
+                        bool isRunning = Process.GetProcessesByName(processName).Any(p => p.MainModule.FileName == program);
+                        if (isRunning)
                         {
-                            statusStrip1.Items[0].Text = "Status: Application '" + processName + "' Closed";
+                            statusStrip1.Items[0].Text = "Status: Application '" + processName + "' Running";
                         }
                         else
                         {
-                            statusStrip1.Items[0].Text = "Status: There is no application running";
+                            var allProcess = Process.GetProcesses();
+
+                            if (allProcess.Length > 0)
+                            {
+                                statusStrip1.Items[0].Text = "Status: Application '" + processName + "' Closed";
+                            }
+                            else
+                            {
+                                statusStrip1.Items[0].Text = "Status: There is no application running";
+                            }
                         }
-                    }
-                };
-                processChecker.Start();
+                    };
+                    processChecker.Start();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An error occurred: '{0}'", ex);
             }
+            _isRunning = false;
         }
 
-        private void textBox1_DoubleClick(object sender, EventArgs e)
+        private void StartListening()
+        {
+            _udpClient = new UdpClient(5555);
+            _remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5555);
+
+            _isListening = true;
+            new Thread(() =>
+            {
+                while (_isListening)
+                {
+                    try
+                    {
+                        _udpClient.BeginReceive(DataReceived, null);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error occurred: '{0}'", ex);
+                    }
+                }
+            }).Start();
+        }
+
+        private void DataReceived(IAsyncResult asyncResult)
+        {
+            if (_udpClient != null && _remoteEndPoint != null)
+            {
+                byte[] receivedBytes = _udpClient.EndReceive(asyncResult, ref _remoteEndPoint);
+                string receivedMessage = Encoding.ASCII.GetString(receivedBytes);
+                dataInBox.Invoke((MethodInvoker)delegate
+                {
+                    dataInBox.AppendText(receivedMessage);
+                });
+            }
+        }
+        #endregion
+
+        /*#region Button Disable
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if (vkCode == (int)Keys.LWin || vkCode == (int)Keys.RWin || vkCode == (int)Keys.Tab && (Control.ModifierKeys & Keys.Alt) != 0)
+                {
+                    return (IntPtr)1;
+                }
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+        #endregion*/
+
+        #region Form Event
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // UnhookWindowsHookEx(_hookID);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _isListening = false;
+            _udpClient.Close();
+        }
+        #endregion
+
+        #region Button Event
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            if (_isRunning) return;
+            StartProcess();
+        }
+
+        private void receiveButton_Click(object sender, EventArgs e)
+        {
+            if (_isListening) return;
+            StartListening();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            dataInBox.Clear();
+        }
+        #endregion
+
+        #region Text Box Event
+        private void pathBox_DoubleClick(object sender, EventArgs e)
         {
             using (OpenFileDialog)
             {
@@ -82,9 +223,16 @@ namespace DesktopRemoteApplication
 
                 if (OpenFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    textBox1.Text = OpenFileDialog.FileName;
+                    pathBox.Text = OpenFileDialog.FileName;
                 }
             }
         }
+
+        private void pathBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_isRunning) return;
+            StartProcess();
+        }
+        #endregion           
     }
 }
